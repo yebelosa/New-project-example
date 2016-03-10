e2980c122350916a5372c1c4defdd573a216cbfa
mattia.battiston <mattia.battiston@gmail.com>
2016-03-10 16:07:38 +0000

    Setting up the project

---
+++ src/main/java/com/mattia/example/Greeter.java
@@ -0,0 +1,9 @@
+package com.mattia.example;
+
+public class Greeter {
+
+    public String greet(String name) {
+        return "Hello " + name + "!";
+    }
+
+}
---
+++ src/test/java/com/mattia/example/GreeterTest.java
@@ -0,0 +1,18 @@
+package com.mattia.example;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GreeterTest {
+
+    @Test
+    public void greetsByName() throws Exception {
+        Greeter greeter = new Greeter();
+
+        String message = greeter.greet("Mattia");
+
+        assertThat(message).isEqualTo("Hello Mattia!") ;
+    }
+
+}
