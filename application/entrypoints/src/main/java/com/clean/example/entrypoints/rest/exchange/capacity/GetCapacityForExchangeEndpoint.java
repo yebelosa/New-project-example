28e2bb60f2817e96d24c7c342f0c02f46aec5367
mattia.battiston <mattia.battiston@gmail.com>
2016-04-05 22:10:18 +0100

    Prepared failing end-to-end test for retrieving capacity in an exchange.

---
+++ application/entrypoints/src/main/java/com/clean/example/entrypoints/rest/exchange/capacity/GetCapacityForExchangeEndpoint.java
@@ -0,0 +1,5 @@
+package com.clean.example.entrypoints.rest.exchange.capacity;
+
+public class GetCapacityForExchangeEndpoint {
+    public static final String API_PATH = "/exchange/{exchangeCode}/capacity";
+}
