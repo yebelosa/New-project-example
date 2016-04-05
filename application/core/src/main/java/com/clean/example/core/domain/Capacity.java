87910ba583efbf9bdce785f358732f86f725ba35
Mattia Battiston <dev@alpha.local>
2016-04-05 13:07:43 +0100

    Sketchin out the new use case.

---
+++ application/core/src/main/java/com/clean/example/core/domain/Capacity.java
@@ -0,0 +1,8 @@
+package com.clean.example.core.domain;
+
+public class Capacity {
+
+    private boolean fiber;
+    private boolean data;
+
+}
---
+++ application/core/src/main/java/com/clean/example/core/usecase/exchange/getcapacity/GetCapacityForExchangeUseCase.java
@@ -0,0 +1,18 @@
+package com.clean.example.core.usecase.exchange.getcapacity;
+
+import com.clean.example.core.domain.Capacity;
+
+public class GetCapacityForExchangeUseCase {
+
+    public Capacity getCapacity(String exchangeCode) {
+        // Exchange exchange = getExchangeFromModel(exchangeCode);
+        // loop(exchange.getDevices()) {
+        // int ports = device.getAvailablePorts();
+        // add to total for bb or fibre
+        // }
+        // decide capacity
+        // needs at least 5 available ports
+        return null;
+    }
+
+}
